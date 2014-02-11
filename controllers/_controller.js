6167b88a54fa83588bad24619fe968d8dee6afa7
Artem Kholodenko <artemk@gmail.com>
2014-02-11 00:12:02 -0800

    styling for categories for search results; display totals of categories across search results

--- controllers/_controller.js
+++ controllers/_controller.js
@@ -5,3 +5,17 @@ var dataSearchApp = angular.module('dataSearchApp', [
 	// depedent modules
 	'ui.keypress'
 ]);
+
+/**	replace titles of all categories	*/
+dataSearchApp.filter('categoryFilter', function() {
+	return function(categories) {
+		var filtered = [];
+
+		angular.forEach(categories, function(category) {
+			category.title = category.title.replace('Category:','');
+			filtered.push(category);
+		});
+
+		return filtered;
+	};
+});
--- controllers/searchController.js
+++ controllers/searchController.js
@@ -1,35 +1,36 @@
 dataSearchApp.controller('searchController', ['$scope', '$http', function ($scope, $http) {
-	/*
-	$scope.phones = [
-		{'name': 'Nexus S',
-			'snippet': 'Fast just got faster with Nexus S.'},
-		{'name': 'Motorola XOOM™ with Wi-Fi',
-			'snippet': 'The Next, Next Generation tablet.'},
-		{'name': 'MOTOROLA XOOM™',
-			'snippet': 'The Next, Next Generation tablet.'}
-	];
-	*/
-
 	$scope.searchResults = [];
 	$scope.searchResultCount = 0;
+	$scope.searchResultCategoryCounts = [];
 
 	$scope.submitSearch = function($event) {
-		console.log(this.keyword);
-
 		if(this.keyword.length) {
-			$http.jsonp('http://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=intitle:' + encodeURIComponent(this.keyword)+ '&format=json&callback=JSON_CALLBACK').
-				success(function(data, status, headers, config) {
-					//console.log(data);
-					$scope.searchResultCount = data.query.searchinfo.totalhits;
-					$scope.searchResults = data.query.search;
+			$scope.searchState = 'Loading...';
+			$scope.searchResults = [];
+			$scope.searchResultCategoryCounts = [];	// clear category counts before search
 
-					// this callback will be called asynchronously
-					// when the response is available
+			$http.jsonp('http://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=intitle:' + encodeURIComponent(this.keyword)+ '&format=json&limit=50&callback=JSON_CALLBACK').
+				success(function(data, status, headers, config) {
+					if(data.query !== undefined) {
+						$scope.searchResultCount = data.query.searchinfo.totalhits;
+						$scope.searchResults = data.query.search;
+
+						// loop through to create wiki page links based on titles
+						for (var i = $scope.searchResults.length - 1; i >= 0; i--) {
+							$scope.searchResults[i].wikiUrl = 'http://en.wikipedia.org/wiki/' + encodeURIComponent($scope.searchResults[i].title);
+						}
+
+						$scope.searchState = '';
+					}
+					// error handling for bad response
+					else {
+						$scope.searchResultCount = 0;
+						$scope.searchState = 'Bad response';
+						$scope.searchResults = [];
+					}
 				}).
 				error(function(data, status, headers, config) {
 					console.log('problem calling Wikipedia API');
-					// called asynchronously if an error occurs
-					// or server returns response with an error status.
 				}
 			);
 		}
@@ -39,36 +40,50 @@ dataSearchApp.controller('searchController', ['$scope', '$http', function ($scop
 	};
 
 	// listen to change in search results and get wikipedia pages for new items
-	$scope.$watch('searchResults', function (newVal, oldVal) {
-		$scope.searchResults.forEach(function(item) {
-			//console.log(item.title);
-			$http.jsonp('http://en.wikipedia.org/w/api.php?action=query&titles=' + encodeURIComponent(item.title) + '&prop=categories&format=json&callback=JSON_CALLBACK').
-				success(function(data, status, headers, config) {
-					var pageId = Object.keys(data.query.pages)[0];
-					//console.log(Object.keys(data.query.pages)[0]);
+	$scope.$watch('searchResults', function (newValue, oldvValue) {
+		// because watch fires upon init, need to make sure the trigger is legit for change in value
+		if(oldvValue.length == newValue.length) {
+			console.log('same val: ' + newValue.length);
+			return;
+		}
+		else {
+			console.log('here');
+			$scope.searchResults.forEach(function(item) {
+				$http.jsonp('http://en.wikipedia.org/w/api.php?action=query&titles=' + encodeURIComponent(item.title) + '&prop=categories&format=json&callback=JSON_CALLBACK').
+					success(function(data, status, headers, config) {
+						var pageId = Object.keys(data.query.pages)[0];
 
-					console.log(data.query.pages[pageId].categories);
-					/*
-					for(var key in data.query.pages) {
-						console.log(data.query.pages[key]);
+						//console.log(data.query.pages[pageId].categories);
+						if(data.query.pages[pageId].categories !== undefined) {
+							item.categories = data.query.pages[pageId].categories;
+
+							for (var i = item.categories.length - 1; i >= 0; i--) {
+								item.categories[i].title = item.categories[i].title.replace('Category:','');
+
+								var found = false;
+								$scope.searchResultCategoryCounts.forEach(function(setCategory) {
+									if(setCategory.title == item.categories[i].title) {
+										setCategory.count++;
+										found = true;
+										return;
+									}
+								});
+
+								if(!found) {
+									//console.log('first time: ' + item.categories[i].title);
+									var category = new Object();
+									category.title = item.categories[i].title;
+									category.count = 1;
+									$scope.searchResultCategoryCounts.push(category);
+								 }
+							}
+						}
+					}).
+					error(function(data, status, headers, config) {
+						console.log('problem calling Wikipedia API (page resolution)');
 					}
-					*/
-/*
-					data.query.pages.forEach(function(itemCategories) {
-						item.categories=itemCategories.categories;
-					});
-					*/
-					item.categories = data.query.pages[pageId].categories;
-
-					// this callback will be called asynchronously
-					// when the response is available
-				}).
-				error(function(data, status, headers, config) {
-					console.log('problem calling Wikipedia API (page resolution)');
-					// called asynchronously if an error occurs
-					// or server returns response with an error status.
-				}
-			);
-		});
+				);
+			});
+		}
 	}, true);
 }]);

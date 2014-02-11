eb5f8379c1566b208f322062ad8ee0c40890a729
Artem Kholodenko <artemk@gmail.com>
2014-02-10 16:36:25 -0800

    added ability to hit wikipedia API to get search results, parse, and get each item’s page to look up categories

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -1,4 +1,5 @@
-dataSearchApp.controller('searchController', function ($scope) {
+dataSearchApp.controller('searchController', ['$scope', '$http', function ($scope, $http) {
+	/*
 	$scope.phones = [
 		{'name': 'Nexus S',
 			'snippet': 'Fast just got faster with Nexus S.'},
@@ -7,20 +8,67 @@ dataSearchApp.controller('searchController', function ($scope) {
 		{'name': 'MOTOROLA XOOM™',
 			'snippet': 'The Next, Next Generation tablet.'}
 	];
+	*/
+
+	$scope.searchResults = [];
+	$scope.searchResultCount = 0;
 
 	$scope.submitSearch = function($event) {
 		console.log(this.keyword);
 
-		$http({method: 'GET', url: '/someUrl'}).
-			success(function(data, status, headers, config) {
-				// this callback will be called asynchronously
-				// when the response is available
-			}).
-			error(function(data, status, headers, config) {
-				// called asynchronously if an error occurs
-				// or server returns response with an error status.
-			});
-		
+		if(this.keyword.length) {
+			$http.jsonp('http://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=intitle:' + encodeURIComponent(this.keyword)+ '&format=json&callback=JSON_CALLBACK').
+				success(function(data, status, headers, config) {
+					//console.log(data);
+					$scope.searchResultCount = data.query.searchinfo.totalhits;
+					$scope.searchResults = data.query.search;
+
+					// this callback will be called asynchronously
+					// when the response is available
+				}).
+				error(function(data, status, headers, config) {
+					console.log('problem calling Wikipedia API');
+					// called asynchronously if an error occurs
+					// or server returns response with an error status.
+				}
+			);
+		}
+
+
 		$event.preventDefault();
 	};
-});
+
+	// listen to change in search results and get wikipedia pages for new items
+	$scope.$watch('searchResults', function (newVal, oldVal) {
+		$scope.searchResults.forEach(function(item) {
+			//console.log(item.title);
+			$http.jsonp('http://en.wikipedia.org/w/api.php?action=query&titles=' + encodeURIComponent(item.title) + '&prop=categories&format=json&callback=JSON_CALLBACK').
+				success(function(data, status, headers, config) {
+					var pageId = Object.keys(data.query.pages)[0];
+					//console.log(Object.keys(data.query.pages)[0]);
+
+					console.log(data.query.pages[pageId].categories);
+					/*
+					for(var key in data.query.pages) {
+						console.log(data.query.pages[key]);
+					}
+					*/
+/*
+					data.query.pages.forEach(function(itemCategories) {
+						item.categories=itemCategories.categories;
+					});
+					*/
+					item.categories = data.query.pages[pageId].categories;
+
+					// this callback will be called asynchronously
+					// when the response is available
+				}).
+				error(function(data, status, headers, config) {
+					console.log('problem calling Wikipedia API (page resolution)');
+					// called asynchronously if an error occurs
+					// or server returns response with an error status.
+				}
+			);
+		});
+	}, true);
+}]);
